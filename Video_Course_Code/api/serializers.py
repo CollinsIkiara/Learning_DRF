72b8a4e9be1a727368d5b8558493b4a4afb5cb5e
Collins Ikiara <>
2026-09-08 22:28:51 +0300

    Add Nested Serializers, the SerializerMethodField and Serializer Relations.

--- Video_Course_Code/api/serializers.py
+++ Video_Course_Code/api/serializers.py
@@ -18,3 +18,52 @@ class ProductSerializer(serializers.ModelSerializer):
                 "Price must be greater than 0."
             )
         return value
+    
+    
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(
+        source='product.price',
+        max_digits=10,
+        decimal_places=2
+    )
+    
+    class Meta:
+        model = OrderItem
+        fields = (
+            'product_name',
+            'product_price',
+            'quantity',
+            'item_subtotal' # This is a property method in the OrderItem model that calculates the subtotal for each order item based on the product price and quantity.
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'date_joined'
+        )
+
+# The OrderSerializer implements nested serialization for the Order model, including related User and OrderItem data. It also calculates the total price of the order using a custom method.
+class OrderSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True) # many=True because a user can have multiple orders, and read_only=True because we don't want to allow the user to be modified through the order serializer.
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total') # SerializerMethodField is used to add a custom field to the serializer that is not directly tied to a model field.
+    
+    def total(self, obj):
+        order_items = obj.items.all() # Get all order items for the order
+        return sum(order_item.item_subtotal for order_item in order_items) # Calculate the total price by summing the item subtotals
+    
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'created_at',
+            'user',
+            'status',
+            'items',
+            'total_price'
+            )
--- Video_Course_Code/api/views.py
+++ Video_Course_Code/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer, OrderSerializer
+from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -15,4 +15,10 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk) # get the product with the given primary key
     serializer = ProductSerializer(product) # serialize the product
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
