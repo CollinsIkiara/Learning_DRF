ec2fd3f733f3eb90cad6808bf78100fb75b4c656
Collins Ikiara <>
2026-09-08 16:29:13 +0300

    Add serializers and response objects
    Created a serializer for the Products model as well as its corresponding function-based views in order to display the JSON responses. Added their paths in urls.py in order to call the views created to a URL to observe on DRF's Browsable API.

--- Video_Course_Code/api/views.py
+++ Video_Course_Code/api/views.py
@@ -1,1 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from api.serializers import ProductSerializer
+from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all() # queryset of all products
+    serializer = ProductSerializer(products, many=True) # many =True because we are serializing a queryset of multiple products
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk) # get the product with the given primary key
+    serializer = ProductSerializer(product) # serialize the product
+    return Response(serializer.data)
